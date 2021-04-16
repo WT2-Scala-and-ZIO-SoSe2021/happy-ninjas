8bc729aceabea357e340dff095a9af28058591e8
ubiquitousbyte <nasdenkov@gmail.com>
2021-04-16 13:35:57 +0200

    abstract over int

--- src/main/scala/IntHelper.scala
+++ src/main/scala/IntHelper.scala
@@ -1,17 +1,22 @@
+import Ordering.Implicits._
 object IntHelper {
 
   private def filter[T: Ordering](arr: Array[T], f: (T, T) => Boolean): T =
     arr.reduceLeft { (prev, current) => if (f(prev, current)) prev else current }
 
+  private def greaterThan[T: Ordering](a: T, b: T): Boolean = a > b
+
+  private def lessThan[T: Ordering](a: T, b: T): Boolean = !greaterThan(a, b)
+
   def max(arr: Array[Int]): Int = {
     // It would be better if the function could return an Either[E, A] to preserve the error as a type, instead of generating a side effect
     if (arr.isEmpty) throw new IllegalArgumentException("Array cannot be empty")
-    else filter(arr, (a: Int, b: Int) => a > b)
+    else filter(arr, greaterThan[Int])
   }
 
   def min(arr: Array[Int]): Int =
     if (arr.isEmpty) throw new IllegalArgumentException("Array cannot be empty")
-    else filter(arr, (a: Int, b: Int) => a < b)
+    else filter(arr, lessThan[Int])
 
   def sum(arr: Array[Int]): Int = {
     if (arr.isEmpty) 0
