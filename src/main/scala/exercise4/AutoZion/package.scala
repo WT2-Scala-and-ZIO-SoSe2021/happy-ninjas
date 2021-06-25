5898ed3397e95bbaa68a6079819ee134e3f39a82
Tilman Möller <moeller.tilman@gmail.com>
2021-06-25 13:21:24 +0200

    job board for the robots as a zlayer

--- src/main/scala/exercise4/AutoZion/package.scala
+++ src/main/scala/exercise4/AutoZion/package.scala
@@ -6,7 +6,7 @@ import zio.duration.Duration
 package object AutoZion {
 
   // TODO: required custom ZIO environment
-  type MyEnv = ???
+  type MyEnv = Has[JobBoard]
 
   trait Robot {
     val name: String
@@ -19,5 +19,39 @@ package object AutoZion {
   case class PendingJob(duration: Duration) extends Job
 
   case class CompletedJob(completedBy: Robot) extends Job
+
+  trait JobBoard {
+    /**
+     * Submits a job to the job board, which can later be taken up by a robot using the take method.
+     */
+    def submit(job: PendingJob): UIO[Unit]
+
+    /**
+     * Take a job from the job board
+     */
+    def take(): UIO[PendingJob]
+  }
+
+  object JobBoard {
+    def submit(job: PendingJob): URIO[Has[JobBoard], Unit] = ZIO.serviceWith[JobBoard](_.submit(job))
+
+    def take(): URIO[Has[JobBoard], PendingJob] = ZIO.serviceWith[JobBoard](_.take())
+  }
+
+  case class JobBoardLive() extends JobBoard {
+    val unboundedQueue: UIO[Queue[PendingJob]] = Queue.unbounded[PendingJob]
+
+    override def submit(job: PendingJob): UIO[Unit] = unboundedQueue.map(value => value.offer(job))
+
+    override def take(): UIO[PendingJob] = for {
+      queue <- unboundedQueue
+      fib_job <- queue.poll
+      job = fib_job.get
+    } yield job
+  }
+
+  object JobBoardLive {
+    val layer: ULayer[Has[JobBoard]] = ZLayer.succeed(JobBoardLive())
+  }
 
 }
